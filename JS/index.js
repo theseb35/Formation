7c8ab764b4547a2e517986cdfc81f9dfc78ad52c
theseb35 <theseb35@live.fr>
2023-06-27 09:47:45 +0200

    calage

--- JS/index.js
+++ JS/index.js
@@ -9,6 +9,10 @@
     console.log(currentDate.toISOString());
     var footer=document.getElementsByTagName('footer')[0]
     footer.innerHTML=currentDate.toISOString()
+    footer.style.color='white'
+    footer.style.backgroundColor='green'
+    footer.style.fontStyle
+
 
   }
   document.addEventListener('DOMContentLoaded',function(evt){
