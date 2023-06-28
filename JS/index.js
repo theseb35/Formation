900e51ee08601afe233dd7996512d15053f281ef
theseb35 <theseb35@live.fr>
2023-06-28 10:59:07 +0200

    recup chemin

---
+++ JS/index.js
@@ -0,0 +1,4 @@
+import {handleRoute} from "./router.js"
+document.addEventListener('DOMContentoaded',(evt)=>{
+    handleRoute()
+})
---
+++ JS/router.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * variable de config des routes
+ */
+const routeConfig = {
+  routes: [],
+};
+/**
+ * fonction de maintien de la route
+ */
+export const handleRoute = () => {
+  const pathname = location.pathname;
+  console.log(pathname);
+};
